3c2f50e774447f8c22d339d38ee7ede8cd9b1a8d
Darren Yin <darrenyin0110@yahoo.com>
2014-09-13 14:19:30 -0400

    adding more functionality to popup.js

--- popup.js
+++ popup.js
@@ -1,37 +1,95 @@
 var kitten = {
-  mood:"Happy", 
-  health:50, 
-  name:"Sir Fluffykins",
-  state:1
-};
-
-var addKitten = function(){
-};
-
-var updateKitten = function(){
-  $("#kittyPic").attr("src", "kitten-gray.jpg");
-};
-
-var sleepAndWake = function(){
-  if (isBlocking == true){
-    $("#powerPic").attr("src", "power_red.png");
-    $('#kittyPic').attr("src", "");
+  mood:0,
+    // 0 = happy
+    // 1 = neutral
+    // 2 = disappointed
+    // 3 = sads
+    // 4 = dedz
+    health:50, 
+    name:"Sir Fluffykins",
+    mode:0,
+    // 0 = glasses
+    // 1 = party
+    // 2 = sleep
+    // 3 = normal
+    powerState:0
   }
-  else{
-    $("#powerPic").attr("src", "power_green.png");
-    $('#kittyPic').attr("src", "kitten-gray.jpg");
-  }
-  switchBlockingOnOff();
-};
 
+  var addKitten = function(){
+    switch(kitten.mood){
+      case 0:
+      $("#kittyPic").attr("src", "placeholder.jpg");
+      break;
+      case 1:
+      $("#kittyPic").attr("src", "power_green.png");  
+      break;
+      case 2:
+      $("#kittyPic").attr("src", "power_red.png");
+      break;
+      case 3:
+      $("#kittyPic").attr("src", "power_gray.jpg");
+      break;
+      case 4:
+      $("#kittyPic").attr("src", "kitten-gray.jpg");
+      break;
+    }
+  };
+
+  var addPowerButton = function(){
+    if (kitten.powerState){
+      $("#powerPic").attr("src", "power_green.png");
+    }
+    else{
+      $("#powerPic").attr("src", "power_gray.png");
+    }
+
+  };
+
+  var addMode = function(){
+    switch(kitten.mode){
+      case 0:
+      break;
+      case 1:
+      break;
+    }
+    $("#modePic").attr("src", "");
+  };
+
+  var hoverPower = function(){
+    $("#powerPic").attr("src", "power_gray.png");
+  };
+
+  var sleepAndWake = function(){
+    if (isBlocking == true){
+      $("#powerPic").attr("src", "power_gray.png");
+      $('#kittyPic').attr("src", "");
+    }
+    else{
+      $("#powerPic").attr("src", "power_green.png");
+      $('#kittyPic').attr("src", "kitten-gray.jpg");
+    }
+    switchBlockingOnOff();
+  };
+
+// MIGHT BE ANDY'S FUNCTION THAT DOES openSettings
 var openSettings = function(){
 };
 
 $(document).ready(function(){
-  $(document).on('click', "#kittyPic", function(){
-    updateKitten();
+  addKitten();
+  addPowerButton();
+  addMode();
+
+  $(document).on('hover', "#powerPic", function(){
+    hoverPower();
   })
   .on('click', "#powerPic", function(){
     sleepAndWake();
+  })
+  .on('hover', "#settingsPic", function(){
+    hoverSetting();
+  })
+  .on('click', "#settingsPic", function(){
+    openSettings();
   });
 });
